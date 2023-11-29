1c6d0b8b27e6170bfbae8b8942cb841294bf8b87
isaac miao <isaacandymiao@gmail.com>
2023-11-28 21:14:22 -0800

    Basic Login, Home, and App.tsx changed BroswerRouter to Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { UserContextProvider } from './context/user-context-provider';
 import Home from './components/Home';
 import User from './components/User';
-import { Routes, Navigate, Route } from 'react-router';
 import Chats from './components/Chats';
 import Login from './components/Login';
 import OptionsBar from './components/OptionsBar';
 
 class App extends React.Component<{}, {}> {
-
   render() {
     return (
       <UserContextProvider>
-        <BrowserRouter>
+        <Router>
           <OptionsBar />
           <Routes>
-            <Route path="/home" element={<Home/>} />
-            <Route path="/user" element={<User />} />
-            <Route path="/chats" element={<Chats />} />
-            <Route path="/login" element={<Login />} />
-            {/* <Route path="*" element={<Navigate to="/home" />} /> */}
+            <Route path="home" element={<Home />} />
+            <Route path="user" element={<User />} />
+            <Route path="chats" element={<Chats />} />
+            <Route path="login" element={<Login />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </UserContextProvider>
     );
   }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -11,51 +11,88 @@ import {
   Button
 } from "reactstrap";
 import { UserContext } from "../context/user-context";
-import { redirect } from "react-router";
 
 const Login: React.FunctionComponent = (): JSX.Element => {
   const context = useContext(UserContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSumbit = (e: any) => {
+  const handleSumbit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    alert("Email: " + email + "\nPassword: " + password);
-    context.updateState({user: {isloggedin: true, firstname: "John", lastname: "Wick", email: email}});
-  }
 
-  return <div className='d-flex justify-content-center'>
-    <Card color="dark" outline style={{width: '36rem'}}>
-      <CardHeader>
-        Rumeez Account Login
-      </CardHeader>
-      <CardBody>
-        <CardTitle tag="h5">
-          Login Form
-        </CardTitle>
-        <Form onSubmit={handleSumbit}>
-          <FormGroup>
-            <Label for="exampleEmail">Email</Label>
-            <Input type="email" name="email" placeholder="jwick@continental.com"
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }} />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Password</Label>
-            <Input type="password" name="password" placeholder="babayaga" 
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }} />
-          </FormGroup>
-          <Button type="submit" value="submit" color="primary">
-                Login
-          </Button>
-        </Form>
-      </CardBody>
-    </Card>
-    <p>Logged in guy: {context.user.firstname ? context.user.firstname : ""}</p>
-  </div>
-}
+    try {
+      const response = await fetch('http://localhost:8000/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      // Handle the response data as needed
+      console.log('Login successful:', data);
+
+      // Update the context with the user information
+      context.updateState({
+        user: {
+          isloggedin: true,
+          firstname: data.firstname, // Adjust according to the actual response structure
+          lastname: data.lastname,   // Adjust according to the actual response structure
+          email: email,
+        },
+      });
+    } catch (error) {
+      console.error('Error during login:', error);
+      // Handle the error, e.g., display an error message to the user
+    }
+  };
+
+  return (
+    <div className='d-flex justify-content-center'>
+      <Card color="dark" outline style={{ width: '36rem' }}>
+        <CardHeader>Rumeez Account Login</CardHeader>
+        <CardBody>
+          <CardTitle tag="h5">Login Form</CardTitle>
+          <Form onSubmit={handleSumbit}>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                placeholder="jwick@continental.com"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                placeholder="babayaga"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </FormGroup>
+            <Button type="submit" value="submit" color="primary">
+              Login
+            </Button>
+          </Form>
+        </CardBody>
+      </Card>
+      <p>Logged in guy: {context.user.firstname ? context.user.firstname : ""}</p>
+    </div>
+  );
+};
 
 export default Login;
